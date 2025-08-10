766d8b7da724a7fe7eae862688f23d479a9d5f4b
Mustfa2006 <www.mustfaabd2006@gmail.com>
2025-08-10 15:22:38 +0300

    151451

--- frontend/lib/pages/advanced_orders_management_page.dart
+++ frontend/lib/pages/advanced_orders_management_page.dart
@@ -140,7 +140,7 @@ class _AdvancedOrdersManagementPageState
 
       _applyFilters();
       if (!loadMore) {
-        _calculateStatistics();
+        await _calculateStatistics();
       }
     } catch (e) {
       setState(() {
